a0fd6a1f14e36982c873e8a35435bfef92ac0469
Haoyang Chen <70962093+Haoyang-Chen@users.noreply.github.com>
2023-08-17 16:16:51 +0800

    modify setting

--- config/setting.py
+++ config/setting.py
@@ -5,7 +5,7 @@ import numpy as np
 
 MINUTES = 60000000000
 TIMESTEP = np.timedelta64(10 * MINUTES)
-PICKUPTIMEWINDOW = np.timedelta64(1000 * MINUTES)
+PICKUPTIMEWINDOW = np.timedelta64(1000 * MINUTES)  # neglect reposition node
 
 # It can enable the neighbor car search system to determine the search range according to the set search distance and
 # the size of the grid. It use dfs to find the nearest idle vehicles in the area.
@@ -21,12 +21,10 @@ if FocusOnLocalRegion == False:
 
 # Input parameters
 VehiclesNumber = 500
-SideLengthMeter = 1200       # 800
+SideLengthMeter = 1200
 VehiclesServiceMeter = 800
 
 DispatchMode = "Simulation"
 DemandPredictionMode = "None"
-# ["TransportationClustering","KmeansClustering","SpectralClustering"]
 ClusterMode = "Grid"
 
-# Preference = True
